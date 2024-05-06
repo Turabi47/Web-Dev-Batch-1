b5b566f91ab58433faf31038d5290960e900d913
Turabi47 <aktura47@gmail.com>
2024-05-06 18:42:32 +0500

    sdkf

--- New file.py
+++ New file.py
@@ -1,11 +1,11 @@
 #It's the new file created in vs code
 print("Hello, world")
 print("Updated code by branch  sdfjs")
-<<<<<<< HEAD
+#<<<<<<< HEAD
 print("Branch 3")
 
-=======
+# =======
 print("here i am"*20)
 print("here i am"*20)
-print("here i am"*20)
->>>>>>> checkbranch
+
+# >>>>>>> checkbranch
