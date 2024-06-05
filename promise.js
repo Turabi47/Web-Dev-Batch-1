f89c956a245dee2b08b3fdb962530c4a9017cdd2
Turabi47 <aktura47@gmail.com>
2024-06-05 22:18:46 +0500

    dfk

--- promise.js
+++ promise.js
@@ -1,44 +1,44 @@
 
-// function fetchData(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             // simulate a random success or error
-//             const isSuccess = Math.random() < 0.7 // 70% chance of success
-//             if (isSuccess){
-//                 const data = {id:2, Name:"Jhon"};
-//                 resolve(data);
-//             }
-//             else{reject('failed to fetch data. Please try again')};
-//         },  2000);  // 2 second delay
-//     });
-// }
+function fetchData(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            // simulate a random success or error
+            const isSuccess = Math.random() < 0.7 // 70% chance of success
+            if (isSuccess){
+                const data = {id:2, Name:"Jhon"};
+                resolve(data);
+            }
+            else{reject('failed to fetch data. Please try again')};
+        },  2000);  // 2 second delay
+    });
+}
 
-// //  Using the fetch data function
+//  Using the fetch data function
 
-// fetchData()
-//     .then((data)=>{
-//         console.log("Data fetched successfully:", data);
+fetchData()
+    .then((data)=>{
+        console.log("Data fetched successfully:", data);
         
-//     })
-//     .catch((error)=>{
-//         console.error('Error:', error)
-//     });
+    })
+    .catch((error)=>{
+        console.error('Error:', error)
+    });
 
-//     //  Example 2
+    //  Example 2
 
-// p = new Promise(function(resolve, reject) {
-//     if (1+1 == 2){
-//         resolve("They are equal");
-//     }else{
-//         reject("They are not equal");
-//     }
-// })
-// p.then((message) => {
-//     console.log(message);
-// },(message) => {
-//     console.log(message);
-// })
-// console.log(typeof(p))
+p = new Promise(function(resolve, reject) {
+    if (1+1 == 2){
+        resolve("They are equal");
+    }else{
+        reject("They are not equal");
+    }
+})
+p.then((message) => {
+    console.log(message);
+},(message) => {
+    console.log(message);
+})
+console.log(typeof(p))
 
 function practice(a){
     return new Promise((resolve, reject) => {
