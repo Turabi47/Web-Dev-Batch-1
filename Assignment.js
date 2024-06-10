b1d95608d36f6ec9dc3ed3eed142decdb9a093b7
Turabi47 <aktura47@gmail.com>
2024-06-10 10:38:18 +0500

    Tried to change in differernt const arrays

--- Assignment.js
+++ Assignment.js
@@ -21,22 +21,22 @@ let obj1 = {
     key1 : "value1", 
     key2 : "value2", 
     key3 : "value3" 
-}
-const obj2 = {
-    key1 : "value1", 
-    key2 : "value2", 
-    key3 : "value3" 
-}
-const obj3 = {
-    key1 : "value1", 
-    key2 : "value2", 
-    key3 : "value3"
-}
-
-Object.freeze(obj2);        // This makes the object unchangeable
-obj2.key1 = "Changed";
-console.log(obj2);
-obj2.key4 = "value4";
+    }
+    const obj2 = {
+        key1 : "value1", 
+        key2 : "value2", 
+        key3 : "value3" 
+        }
+        const obj3 = {
+            key1 : "value1", 
+            key2 : "value2", 
+            key3 : "value3"
+            }
+            
+            Object.freeze(obj2);        // This makes the object unchangeable
+            obj2.key1 = "Changed";
+            console.log(obj2);
+            obj2.key4 = "value4";
 obj2.key2 = delete
 console.log(obj2);
 
@@ -48,6 +48,23 @@ obj3.key4 = "value4";
 obj3.key2 = delete
 console.log(obj3);
 
+//      Assignment No. 3
+
+let arr1 = [1, 2, 3, 4, 5]
+const arr2 = [1, 2, 3, 4, 5]
+const arr3 = [1, 2, 3, 4, 5]
+
+Object.freeze(arr2)
+// arr2.push(6)        // These two lines give an error because the array is immutable
+// arr2.pop()
+arr2[0] = 11
+console.log(arr2)
+
+Object.seal(arr3)
+// arr3.push(6)        This does not work
+// arr3.pop()
+arr3[0] = 11        // But this works
+console.log(arr3)
 
 
 
