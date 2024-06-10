da9bd749f29aeb42e1c764a23348ed669e21465f
Turabi47 <aktura47@gmail.com>
2024-06-10 10:22:27 +0500

    Changes in const objects

--- Assignment.js
+++ Assignment.js
@@ -8,7 +8,7 @@ console.log("The old value of variable declared by 'var':",b)
 console.log("The old value of variable declared by 'const':",c)
 a++;
 b++;
-c++;            //it will give error until we comment this line. This means const variables does not change
+// c++;            //it will give error until we comment this line. This means const variables does not change
 console.log("The new value of variable declared by 'let':",a)
 console.log("The new value of variable declared by 'var':",b)
 console.log("The new value of variable declared by 'const':",c)
@@ -20,5 +20,34 @@ console.log("The new value of variable declared by 'const':",c)
 let obj1 = {
     key1 : "value1", 
     key2 : "value2", 
-    key3 : "value3", 
+    key3 : "value3" 
 }
+const obj2 = {
+    key1 : "value1", 
+    key2 : "value2", 
+    key3 : "value3" 
+}
+const obj3 = {
+    key1 : "value1", 
+    key2 : "value2", 
+    key3 : "value3"
+}
+
+Object.freeze(obj2);        // This makes the object unchangeable
+obj2.key1 = "Changed";
+console.log(obj2);
+obj2.key4 = "value4";
+obj2.key2 = delete
+console.log(obj2);
+
+
+Object.seal(obj3);          // This makes object not add or delete any pair
+obj3.key1 = "Changed";
+console.log(obj3);
+obj3.key4 = "value4";
+obj3.key2 = delete
+console.log(obj3);
+
+
+
+
