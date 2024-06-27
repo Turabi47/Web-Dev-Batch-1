d51a32e0d66c176f44910f7b9b834b65a51269c8
Turabi47 <aktura47@gmail.com>
2024-06-27 18:59:40 +0500

    dfk

--- first.js
+++ first.js
@@ -1,47 +1,47 @@
-// let a = 10
-// console.log(a)
+let a = 10
+console.log(a)
 
-// let b = 34
-// console.log(typeof(b))
-// console.log(b)
+let b = 34
+console.log(typeof(b))
+console.log(b)
 
-// const student = {
-//     Name : "Abdul Aziz",
-//     age : 24,
-//     email : "akturabi@gmail.com"
+const student = {
+    Name : "Abdul Aziz",
+    age : 24,
+    email : "akturabi@gmail.com"
 
-// }
+}
 
-// student.Name = "Abdul Aziz Khan"
-// console.log(student.Name)
+student.Name = "Abdul Aziz Khan"
+console.log(student.Name)
 
-// const Name = "Zahid"
-// // Name = "Zahid Saeed"
-// console.log(Name)
+const Name = "Zahid"
+// Name = "Zahid Saeed"
+console.log(Name)
 
-// var stud = {
-//     Name : "Zahid",
-//     rNo : 123,
-//     age : 23
+var stud = {
+    Name : "Zahid",
+    rNo : 123,
+    age : 23
 
-// }
-// stud.age = 22
-// console.log(stud)
+}
+stud.age = 22
+console.log(stud)
 
-// let c = "sdf"
-// c = 32
+let c = "sdf"
+c = 32
 
-// console.log("Type of c is:",typeof(c))
+console.log("Type of c is:",typeof(c))
 
-// function myfunction() {
-//     console.log(c)
-// }
-// myfunction()
+function myfunction() {
+    console.log(c)
+}
+myfunction()
 
 
-// z = 20
+z = 20
 
-// console.log(z)
+console.log(z)
 
 let y = 0
 const six = [3,4,5,6]
