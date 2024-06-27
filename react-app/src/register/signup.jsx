5a6badb93a6377b035996609d450e3568c11f286
Turabi47 <aktura47@gmail.com>
2024-06-27 18:30:08 +0500

    f

---
+++ react-app/src/register/signup.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function signup() {
+  return (
+    <div>
+      <div className="form">
+    <label htmlFor="User name"></label>
+    <input type="text" placeholder='Type your name'/>
+
+    <label htmlFor="Email address"></label>
+    <input type="Email" placeholder='your email address'/>
+
+      </div>
+    </div>
+  )
+}
